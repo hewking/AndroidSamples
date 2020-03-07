2a32bcc9b03832af4ef8d42a24c4b159d3c01258
hewking <cjh1095358798@gmail.com>
2020-03-07 23:36:57 +0800

    add demoList

--- app/src/main/java/com/hewking/develop/MainActivity.kt
+++ app/src/main/java/com/hewking/develop/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hewking.develop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hewking.develop.demo.DemoListFragment
 import com.hewking.develop.service.CountDownService
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,9 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_start.setOnClickListener {
-            startService(Intent(this@MainActivity,CountDownService::class.java))
-        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frameLayout,DemoListFragment(),"DemoListFragment")
+            .commit()
+//        btn_start.setOnClickListener {
+//            startService(Intent(this@MainActivity,CountDownService::class.java))
+//        }
 
+    }
+
+    override fun onBackPressed() {
+        if(supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
