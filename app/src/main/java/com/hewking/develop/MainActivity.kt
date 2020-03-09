b7f0bfe673143fd1813023e4259a3bb0696b4e1d
hewking <cjh1095358798@gmail.com>
2020-03-09 09:58:42 +0800

    update CountDownService

--- app/src/main/java/com/hewking/develop/MainActivity.kt
+++ app/src/main/java/com/hewking/develop/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.hewking.develop
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import com.hewking.develop.service.CountDownService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var binder : CountDownService.CountDownBinder?= null
+    private var bound = false
+
+    private val connection = object : ServiceConnection{
+        override fun onServiceDisconnected(name: ComponentName?) {
+            bound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            binder = service as CountDownService.CountDownBinder?
+            bound = true
+            Log.d("MainActivity","count:${binder?.getService()?.getCount()}")
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +36,16 @@ class MainActivity : AppCompatActivity() {
             startService(Intent(this@MainActivity,CountDownService::class.java))
         }
 
+        btn_bind.setOnClickListener {
+            Intent(this@MainActivity,CountDownService::class.java).also {
+                bindService(it,connection,Context.BIND_AUTO_CREATE)
+            }
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
     }
 }
--- app/src/main/java/com/hewking/develop/service/CountDownService.kt
+++ app/src/main/java/com/hewking/develop/service/CountDownService.kt
@@ -2,6 +2,7 @@ package com.hewking.develop.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 
@@ -16,6 +17,8 @@ class CountDownService : Service() {
 
     private var count = 10
 
+    private val localBinder = CountDownBinder()
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG,"onStartCommand")
         Worker().start()
@@ -23,7 +26,7 @@ class CountDownService : Service() {
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return localBinder
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
@@ -34,6 +37,8 @@ class CountDownService : Service() {
         super.onDestroy()
         Log.d(TAG,"onDestroy")
     }
+
+    fun getCount() = this.count
 
     private inner class Worker : Thread() {
         override fun run() {
@@ -49,6 +54,12 @@ class CountDownService : Service() {
                 Log.d(TAG,"count:$count")
             }
         }
+    }
+
+    inner class CountDownBinder : Binder() {
+
+        fun getService() = this@CountDownService
+
     }
 
 }
