3cefd16d7ccb255e9eca0722ea6e435215865fa6
hewking <cjh1095358798@gmail.com>
2020-03-20 00:38:36 +0800

    update

--- app/src/main/java/com/hewking/develop/util/ImageLoader.kt
+++ app/src/main/java/com/hewking/develop/util/ImageLoader.kt
@@ -1,2 +1,14 @@
 package com.hewking.develop.util
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+object ImageLoader {
+
+    fun load(imgUrl:String ,target: ImageView) {
+        Glide.with(target)
+            .load(imgUrl)
+            .into(target)
+    }
+
+}
--- nativeapp/src/main/cpp/native-lib.cpp
+++ nativeapp/src/main/cpp/native-lib.cpp
@@ -2,3 +2,13 @@
 // Created by jianhao on 2020/3/15.
 //
 
+#include "com_example_nativeapp_NativeMsg.h"
+#include <jni.h>
+#include <string>
+extern "C"
+JNIEXPORT jstring JNICALL Java_com_example_nativeapp_NativeMsg_getMsg
+        (JNIEnv* env, jobject thiz) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+
+}
