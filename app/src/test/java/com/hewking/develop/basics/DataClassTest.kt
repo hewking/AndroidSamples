4d5999a203bb66d52f3f58d092cc6766cdfc708a
hewking <cjh1095358798@gmail.com>
2020-07-03 19:04:35 +0800

    update

--- app/src/test/java/com/hewking/develop/basics/DataClassTest.kt
+++ app/src/test/java/com/hewking/develop/basics/DataClassTest.kt
@@ -24,6 +24,13 @@ class DataClassTest {
 
         println("p2 :${p2}")
 
+        /**
+         * 经过demo测试 在编写data class 用于gson解析时
+         * 1. 需要一个默认的构造方法，这样就需要所有的 字段都要赋默认值
+         * 2. 不确定会有的的值需要加 ? ,这样服务端没有返回才不会出现使用时报NPE异常
+         * 3. 加 ？的字段，可以不用赋值null,因为?本身就已经有一个null值了
+         */
+
     }
 
     data class Person(val name:String? = "fff",val age: Int = 1) {
