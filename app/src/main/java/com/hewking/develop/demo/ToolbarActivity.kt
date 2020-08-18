68f6c499d0a866438b6bec0f4f0d0c580af2cc53
jianhao <chengjianhao@fcbox.com>
2020-08-18 19:55:44 +0800

    update

--- app/src/main/java/com/hewking/develop/demo/ToolbarActivity.kt
+++ app/src/main/java/com/hewking/develop/demo/ToolbarActivity.kt
@@ -1,8 +1,12 @@
 package com.hewking.develop.demo
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.hewking.develop.R
 import com.hewking.develop.base.BaseActivity
+import com.hewking.develop.util.TransparentToolbarManager
 import kotlinx.android.synthetic.main.fragment_toolbar.*
 
 /**
@@ -10,17 +14,54 @@ import kotlinx.android.synthetic.main.fragment_toolbar.*
  * @create: 2020/8/18
  * @description:
  */
-class ToolbarActivity: BaseActivity(){
+class ToolbarActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_toolbar)
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.run {
+//            setDisplayShowTitleEnabled(false)
+//            setHomeButtonEnabled(true)
+//            setDisplayHomeAsUpEnabled(true)
+//        } ?: kotlin.run {
+//
+//        }
 
-        supportActionBar?.run {
-            setSupportActionBar(toolbar)
-        }?: kotlin.run {
+//        changeToolbar()
+//        toolbar.background.alpha = 0
+        webview.loadUrl("https://h5-mall-sit2.fcbox.com/index.html?show=detail#/index/20700188")
 
+        val toolbarManager = TransparentToolbarManager(toolbar)
+        toolbarManager.setToolbarAlpha(0)
+    }
+
+    private fun changeToolbar() {
+        webview.apply {
+            val lp = layoutParams as ConstraintLayout.LayoutParams
+            lp.topToBottom = ConstraintLayout.LayoutParams.UNSET
+            lp.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
+            layoutParams = lp
         }
+    }
+
+    /**
+     * 响应toolbar 里面的navigationIcon 点击事件
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d("onOptionsItemSelected", item.toString())
+        when (item.itemId) {
+            android.R.id.home -> {
+                val fragmentSize = supportFragmentManager.backStackEntryCount
+                if (0 < fragmentSize) {
+                    supportFragmentManager.popBackStack()
+                } else {
+                    onBackPressed()
+                }
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
---
+++ app/src/main/java/com/hewking/develop/util/TransparentToolbarManager.java
@@ -0,0 +1,57 @@
+package com.hewking.develop.util;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.hewking.develop.R;
+
+/**
+ * @author: jianhao
+ * @create: 2020/8/18
+ * @description:
+ */
+public class TransparentToolbarManager {
+
+    private Toolbar mToolbar;
+    private ColorDrawable colorDrawable;
+    public static final int MAX_ALPHA = 255, MIN_ALPHA = 0;
+
+    public TransparentToolbarManager(Toolbar mToolbar) {
+        this.mToolbar = mToolbar;
+        this.colorDrawable = new ColorDrawable(mToolbar.getContext().getResources().getColor(R.color.colorPrimary));
+    }
+
+    public TransparentToolbarManager(Toolbar mToolbar, ColorDrawable colorDrawable) {
+        this.mToolbar = mToolbar;
+        this.colorDrawable = colorDrawable;
+    }
+
+    //Fading toolbar
+    public void manageFadingToolbar(int scrollDistance) {
+        if (mToolbar != null && colorDrawable != null) {
+            //FadeinAndOut according to the horizontal scrollValue
+            if (scrollDistance <= MAX_ALPHA && scrollDistance >= MIN_ALPHA) {
+                setToolbarAlpha(scrollDistance);
+            } else if (scrollDistance > MAX_ALPHA) {
+                setToolbarAlpha(MAX_ALPHA);
+            }
+        }
+    }
+
+
+    public void setToolbarAlpha(int i) {
+        colorDrawable.setAlpha(i);
+        if (isSupport(16)) {
+            mToolbar.setBackground(colorDrawable);
+        } else {
+            mToolbar.setBackgroundDrawable(colorDrawable);
+        }
+    }
+
+    public static boolean isSupport(int apiLevel) {
+        return Build.VERSION.SDK_INT >= apiLevel;
+    }
+
+}
