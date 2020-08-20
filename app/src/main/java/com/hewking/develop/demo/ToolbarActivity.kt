f1397cba6e2151da12125af3cd3e1177aee786a1
hewking <cjh1095358798@gmail.com>
2020-08-20 17:11:00 +0800

    add toolba 沉浸式状态栏demo

--- app/src/main/java/com/hewking/develop/demo/ToolbarActivity.kt
+++ app/src/main/java/com/hewking/develop/demo/ToolbarActivity.kt
@@ -29,7 +29,7 @@ class ToolbarActivity : BaseActivity() {
         supportActionBar?.run {
             setDisplayShowTitleEnabled(false)
             setHomeButtonEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
+            setDisplayHomeAsUpEnabled(false)
         } ?: kotlin.run {
 
         }
