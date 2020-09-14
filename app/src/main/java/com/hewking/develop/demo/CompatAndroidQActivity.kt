638490964e57efa677abb4425df762ff9e284ea2
jianhao <chengjianhao@fcbox.com>
2020-09-14 19:17:47 +0800

    feat: add get androidId

--- app/src/main/java/com/hewking/develop/demo/CompatAndroidQActivity.kt
+++ app/src/main/java/com/hewking/develop/demo/CompatAndroidQActivity.kt
@@ -3,6 +3,7 @@ package com.hewking.develop.demo
 import android.Manifest
 import android.app.Activity
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -12,7 +13,11 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -36,19 +41,29 @@ import kotlin.concurrent.thread
 
 const val PICK_FILE = 1
 
- class CompatAndroidQActivity: AppCompatActivity() {
+class CompatAndroidQActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compat_androidq)
         val permissionsToRequire = ArrayList<String>()
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsToRequire.add(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsToRequire.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
-        ActivityCompat.requestPermissions(this, permissionsToRequire.toTypedArray(), 0)
+        if (permissionsToRequire.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequire.toTypedArray(), 0)
+        }
         browseAlbum.setOnClickListener {
             val intent = Intent(this, BrowseAlbumActivity::class.java)
             startActivity(intent)
@@ -68,28 +83,49 @@ const val PICK_FILE = 1
         pickFile.setOnClickListener {
             pickFileAndCopyUriToExternalFilesDir()
         }
+
+        btnGetIMEI.setOnClickListener {
+            val androidId = getIMEI()
+            Toast.makeText(this@CompatAndroidQActivity, "androidID:$androidId", Toast.LENGTH_SHORT)
+                .show()
+            Log.d("androidID", "androidID:$androidId")
+        }
+
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 0) {
             for (result in grantResults) {
                 if (result != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "You must allow all the permissions.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You must allow all the permissions.", Toast.LENGTH_SHORT)
+                        .show()
                     finish()
                 }
             }
         }
     }
 
-    private fun addBitmapToAlbum(bitmap: Bitmap, displayName: String, mimeType: String, compressFormat: Bitmap.CompressFormat) {
+    private fun addBitmapToAlbum(
+        bitmap: Bitmap,
+        displayName: String,
+        mimeType: String,
+        compressFormat: Bitmap.CompressFormat
+    ) {
         val values = ContentValues()
         values.put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
         values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
         } else {
-            values.put(MediaStore.MediaColumns.DATA, "${Environment.getExternalStorageDirectory().path}/${Environment.DIRECTORY_DCIM}/$displayName")
+            values.put(
+                MediaStore.MediaColumns.DATA,
+                "${Environment.getExternalStorageDirectory().path}/${Environment.DIRECTORY_DCIM}/$displayName"
+            )
         }
         val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         if (uri != null) {
@@ -104,7 +140,11 @@ const val PICK_FILE = 1
 
     private fun downloadFile(fileUrl: String, fileName: String) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            Toast.makeText(this, "You must use device running Android 10 or higher", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "You must use device running Android 10 or higher",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         thread {
@@ -127,13 +167,17 @@ const val PICK_FILE = 1
                         val buffer = ByteArray(1024)
                         var bytes = bis.read(buffer)
                         while (bytes >= 0) {
-                            bos.write(buffer, 0 , bytes)
+                            bos.write(buffer, 0, bytes)
                             bos.flush()
                             bytes = bis.read(buffer)
                         }
                         bos.close()
                         runOnUiThread {
-                            Toast.makeText(this, "$fileName is in Download directory now.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this,
+                                "$fileName is in Download directory now.",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
                 }
@@ -171,13 +215,14 @@ const val PICK_FILE = 1
         val cursor = contentResolver.query(uri, null, null, null, null)
         if (cursor != null && cursor.count > 0) {
             cursor.moveToFirst()
-            fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+            fileName =
+                cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
             cursor.close()
         }
         return fileName
     }
 
-    private fun  copyUriToExternalFilesDir(uri: Uri, fileName: String) {
+    private fun copyUriToExternalFilesDir(uri: Uri, fileName: String) {
         thread {
             val inputStream = contentResolver.openInputStream(uri)
             val tempDir = getExternalFilesDir("temp")
@@ -196,10 +241,29 @@ const val PICK_FILE = 1
                 bos.close()
                 fos.close()
                 runOnUiThread {
-                    Toast.makeText(this, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG)
+                        .show()
                 }
             }
         }
+    }
+
+    private fun getIMEI(): String? {
+        val manager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        /**
+         * Build.getSerial();
+        TelephonyManager.getImei();
+        TelephonyManager.getMeid()
+        TelephonyManager.getDeviceId();
+        TelephonyManager.getSubscriberId();
+        TelephonyManager.getSimSerialNumber();
+         */
+//         manager.imei
+        var androidId: String? =
+            Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+
+        return androidId
     }
 
 }
