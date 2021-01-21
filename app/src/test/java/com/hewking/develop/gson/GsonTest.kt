4a714d47dfc2986630d92f525713db068d9c15e4
jianhao <chengjianhao@fcbox.com>
2021-01-21 17:33:38 +0800

    update

--- app/src/test/java/com/hewking/develop/gson/GsonTest.kt
+++ app/src/test/java/com/hewking/develop/gson/GsonTest.kt
@@ -2,6 +2,9 @@ package com.hewking.develop.gson
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import org.junit.Before
 import org.junit.Test
 
@@ -23,19 +26,90 @@ class GsonTest {
     fun test(){
         val p = Person()
 
-        val map: Map<*,*> = gson.fromJson(gson.toJson(p),Map::class.java)
+        val map: Map<*, *> = gson.fromJson(gson.toJson(p), Map::class.java)
 
         println(map)
     }
 
     @Test
     fun foo(){
-        val map = mapOf<String,String>("name" to "tom","age" to "12")
-        println(gson.fromJson(gson.toJson(map),Map::class.java))
+        val map = mapOf<String, String>("name" to "tom", "age" to "12")
+        println(gson.fromJson(gson.toJson(map), Map::class.java))
     }
 
-    class Person {
-        var name = "heihei"
+    @Test
+    fun bar(){
+
+        val typeToken = object :TypeToken<List<Person>>(){}.type
+
+        val persons = listOf<Person>(Person(), Person())
+
+        val json = gson.toJson(persons)
+
+        val list = gson.fromJson<List<Person>>(json, List::class.java)
+
+        println("list: $list")
+
+        testJson<List<Person>>()
+    }
+
+    inline fun <reified T> testJson(){
+        print(T::class.java)
+//        gson.fromJson<T>("",T::class.java)
+    }
+
+    /**
+     * @param json list的序列化字符串
+     * @param <T>  T类型
+     * @return List<T>
+    </T></T> */
+    fun <T> toList(json: String?, clazz: Class<T>?): List<T>? {
+        return gson.fromJson(json, TypeToken.getParameterized(MutableList::class.java, clazz).type)
+    }
+
+// 或者
+
+    // 或者
+    /**
+     * @param json list的序列化字符串
+     * @param <T>  T类型
+     * @return List<T>
+    </T></T> */
+    fun <T> parseList(json: String?, clazz: Class<T>?): List<T>? {
+        val list: MutableList<T> = ArrayList()
+        try {
+            val array: JsonArray = JsonParser().parse(json).getAsJsonArray()
+            for (elem in array) {
+                list.add(Gson().fromJson(elem, clazz))
+            }
+        } catch (e: Exception) {
+            return null
+        }
+        return list
+    }
+
+    data class CommonConfigEntity(
+        val configCode: String = "",
+        val configOrder: Int = 0,
+        val id: Int = 0,
+        val parentDesc: String = "",
+        val subLink: String = "",
+        var tagName: String = ""
+    )
+
+    @Test
+    fun baz(){
+        val json = "[{\"configCode\":\"accountsSecurity\",\"configOrder\":1,\"id\":19,\"parentDesc\":\"账户与安全\",\"subLink\":\"fcredirect://generalset_safe_account\",\"tagName\":\"\"},{\"configCode\":\"aboutUs\",\"configOrder\":2,\"id\":20,\"parentDesc\":\"关于我们\",\"subLink\":\"fcredirect://generalset_about_us\",\"tagName\":\"\"},{\"configCode\":\"userAgreement\",\"configOrder\":3,\"id\":21,\"parentDesc\":\"用户协议\",\"subLink\":\"fcredirect://generalset_user_protocol\",\"tagName\":\"\"},{\"configCode\":\"privacyPolicy\",\"configOrder\":4,\"id\":22,\"parentDesc\":\"隐私政策\",\"subLink\":\"fcredirect://generalset_privacy_policy\",\"tagName\":\"\"},{\"configCode\":\"clearCache\",\"configOrder\":5,\"id\":23,\"parentDesc\":\"清理缓存\",\"subLink\":\"fcredirect://generalset_clean_cache\",\"tagName\":\"30MB\"}]"
+
+        val type = object : TypeToken<List<CommonConfigEntity>>(){}.type
+
+        val result = gson.fromJson<List<CommonConfigEntity>>(json, type)
+
+        println("result: $result")
+
+    }
+
+    data class Person(val name: String = "heihei") {
         var age = 13
         var set = 1
     }
